3ef5677cb70108f1226731fae2a4e52b6e657aff
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2024-06-05 18:17:24 +0800

    update device

--- train.py
+++ train.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
     # Define your model, optimizer, and criterion
     model, inp, out_p, out_p_filt = TestNet().build_model()
     minibatch_size = 3
-    sim = nengo_dl.Simulator(model, minibatch_size=minibatch_size)
+    sim = nengo_dl.Simulator(model, minibatch_size=minibatch_size, device="/gpu:2")
 
     factor = args.spatial_factor  # spatial downsample factor
     temp_subsample_factor = args.temporal_subsample_factor  # downsampling original 100Hz label to 20Hz
